fd3153d07a7b513aaa121479347b2a6c03c318ce
M.Mehdi <m.sfj9696@gmail.com>
2021-10-05 04:58:04 +0330

    Add createState and restore method in Editor(Originator)

--- src/com/mehdisarf/memento/Editor.java
+++ src/com/mehdisarf/memento/Editor.java
@@ -6,38 +6,28 @@ import java.util.List;
 public class Editor {
 
     private String content;
-//    private List<EditorState> prevStates = new ArrayList<>();
 
     public String getContent() {
         return content;
     }
 
     public void setContent(String content) {
-
-        //if (this.content == null)
         this.content = content;
-        //else {
-/*
-            // content ke avaz mishe yani mirim be ye state jadid.
-            EditorState currentState = new EditorState(this.content); // createState() ya hamun createMemento() qarare dar ayande in karo bokone.
-            prevStates.add(currentState); // Ye Kar e State Management ii.
-
-            this.content = content;
-        }
- */
     }
 
-/*
-    public void undo() {
-        // tamame in method State Management e.
-        // bejoz khate akhar.
-        // pas har3 khat ro mibarim tu caretaker.
-        int lastIndex = prevStates.size() - 1;
-
-        EditorState lastState = prevStates.get(lastIndex);
-        prevStates.remove(lastIndex);
-
-        this.content = lastState.getContent();
+    // hala mibinam ke method e save() careTaker ye EditorState migire va tuye list state hash zakhire mikone.
+    // pas man bayad ye method dashte basham ke khorak e method save() careTaker ro faraham va tamin bokone.
+    // so:
+    public EditorState createState() {
+        return new EditorState(this.content); // state ro misaze, va dar yek zaman e moshakhas
+        // az ruye 'internal state' e editor, state ro por mikone va barmigardune.
     }
- */
+
+    // zemnan mibnam ke method e pop e careTaker, akharin state, ro barmigardune.
+    // khob in be che dard mikhore? be in dard ke man(yani editor, yani originator) un akharin state, yadegari ro begiram
+    // va internal state am ro restore konam be un state. bargardam be un.
+    // (man e originator enqadr zalil nashodam ke set kardan field ham ro ham yeki dg anjam bede.
+    public void restore(EditorState state) {
+        this.content = state.getContent();
+    }
 }
--- src/com/mehdisarf/memento/StateManager.java
+++ src/com/mehdisarf/memento/StateManager.java
@@ -7,14 +7,12 @@ public class StateManager {
 
     private List<EditorState> prevStates = new ArrayList<>();
 
-    public void save(EditorState currentState) { // in signature ro az ruye khat paeini sakhtam.
-        prevStates.add(currentState); // ye kar SMii ine ke tuye prevStates, state, store koni.
+    public void save(EditorState currentState) {
+        prevStates.add(currentState);
     }
 
-    public EditorState pop() { // in signature ro az ruye khotut paeini sakhtam.
+    public EditorState pop() {
 
-        // ye kar SMii ine ke akharin state zakhire shode tuye prevState ro bargarduni va bad az
-        // list hazfesh koni.
         int lastIndex = prevStates.size() - 1;
         EditorState lastState = prevStates.get(lastIndex);
         prevStates.remove(lastIndex);
